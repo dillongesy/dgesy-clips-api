a4f5570633c0eed20773c31aa76cd0eff8ca349b
DillonGesy <dillon.gesy@yahoo.com>
2026-03-19 22:19:44 -0500

    Initial Spring Boot 4 Scaffold

---
+++ src/main/java/clips_api/ClipsApiApplication.java
@@ -0,0 +1,13 @@
+package clips_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/clips_api/ClipsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package clips_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClipsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
